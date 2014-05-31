a50f204ac21696727defbebb1a706abf64cf59a8
Moudy <moudy.elkammash@gmail.com>
2014-05-31 19:40:00 -0400

    add readme

---
+++ README.md
@@ -0,0 +1,43 @@
+# Agenda UI
+
+A UI to view [Agneda](https://github.com/rschmukler/agenda) jobs.
+
+### This is still very much a work in progress
+It's better than nothing but still pretty rough. I'm open to suggestions and pull requests.
+
+### Install
+```
+npm install --save agenda-ui
+```
+
+### Usage
+The Agenda UI is middleware you can mount at a path in your express app.
+```
+var express = require('express');
+var Agenda = require('agenda');
+var agendaUI = require('agenda-ui');
+
+var app = express();
+var agenda = new Agenda(...)
+
+app.use('/agenda-ui', agendaUI(agenda, {poll: 1000}));
+```
+
+### Documentation
+#### agendaUI(agendaInstance, options)
+The first argument is the agenda instance. The second is an options object. Currently the only options is `poll`. This sets the interval the app polls for chnages. It can also be set to `false` to not poll at all.
+
+
+### Screenshot
+
+### Todo and Ideas
+- browse by name, type, etc.
+- pagination
+- a way to view a specific job
+- general UI polish
+- better dev workflow
+- tests
+
+
+### Developing
+This is an Ember app that gets built on `prepublish`. To try it out locally with some sample jobs run `npm run dev` and go to [http://localhost:3022/agenda-ui](http://localhost:3022/agenda-ui).
--- dev.js
+++ dev.js
@@ -7,24 +7,35 @@ var app = express();
 var agenda = new Agenda({db: { address: 'localhost:27017/agenda-ui-development'}});
 agenda._db.remove(function () {
 
-  agenda.define('delete old users', function(job, done) {
+  agenda.define('send email', function(job, done) {
     console.log('Delete old users', job.attrs.data);
     setTimeout(done, 1000);
   });
 
-  agenda.define('update users', function(job, done) {
+  agenda.define('delete users', function(job, done) {
     console.log('Update users', job.attrs.data);
     setTimeout(done, 3000);
   });
 
-  agenda.define('send email', function(job, done) {
+  agenda.define('run analytics', function(job, done) {
     console.log('Send email', job.attrs.data);
     setTimeout(done, 2000);
   });
 
-  agenda.schedule('in 25 seconds', 'send email', {to: 'admin@example.com'});
-  agenda.every('18 seconds', 'delete old users', { userId: 'foo'});
-  agenda.every('1 minute', 'update users', { userId: 'bar'});
+  agenda.schedule('in 25 seconds', 'send email', {
+    to: 'admin@example.com'
+  , subject: 'Report'
+  });
+
+  agenda.every('18 seconds', 'delete users', {
+    inactive: true
+  , paying: false
+  });
+
+  agenda.every('1 minute', 'run analytics', {
+    type: 'engadgemant'
+  , email: 'team'
+  });
 
   app.use('/agenda-ui/assets', require('broccoli-middleware'));
   app.use('/agenda-ui', agendaUI(agenda, {poll: 1000}));
